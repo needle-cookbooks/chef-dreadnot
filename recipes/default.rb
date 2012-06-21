946e06f64be4f5e0d3ba4b5e04fa28d1fb2feb97
Ben Belchak <ben@needle.com>
2012-06-20 20:19:41 -0600

    Fixing the repository.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,7 @@ include_recipe "apt"
 include_recipe "node::apt"
 include_recipe "base::deploy"
 include_recipe "deploy_wrapper"
+include_recipe "runit"
 
 data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
 secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
@@ -30,7 +31,7 @@ template '/opt/needle/dreadnot/local_settings.js' do
     mode 0750
     owner 'root'
     group 'root'
-    variables( :dreadnot => @node[:dreadnot] )
+    variables( :dreadnot => node[:dreadnot] )
 end
 
 node_npm "dreadnot" do
@@ -40,7 +41,7 @@ end
 runit_service "dreadnot"
 
 deploy "/opt/needle/dreadnot/" do
-    repository "dreadnot-stacks"
+    repository "git@github.com:needle/dreadnot-stacks.git"
     symlinks.clear
     symlink_before_migrate.clear
     create_dirs_before_symlink.clear
