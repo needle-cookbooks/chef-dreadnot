33dc29db181bd00165eecd8756c2399c8cb34787
Ben Belchak <ben@needle.com>
2012-09-11 16:35:45 -0600

    adding graphite params to the config

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,5 @@ default[:dreadnot][:host] = "localhost"
 default[:dreadnot][:github_org] = "needle"
 default[:dreadnot][:knife_path] = "/usr/local/bin/knife"
 default[:dreadnot][:ssh_control_persist] = "4h"
+default[:dreadnot][:graphite_host] = "localhost"
+default[:dreadnot][:graphite_port] = 2003
