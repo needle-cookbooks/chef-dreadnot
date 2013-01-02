114564da0b67a7e94649d897df8962e641d5fb83
Cameron Johnston <cameron@needle.com>
2013-01-02 15:08:02 -0700

    bump to 0.0.4

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer  "Needle Ops"
 maintainer_email "ops@needle.com"
 description "Deploys and configures Dreadnot from Rackspace"
 license "Apache 2.0"
-version "0.0.3"
+version "0.0.4"
 
 %w{ apt base deploy_wrapper node runit aws secrets discovery }.each do |cb|
   depends cb
