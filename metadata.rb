0182c3893b1a00e4568503119cb5c7c8694a3012
Ben Belchak <ben@needle.com>
2013-05-31 11:07:00 -0600

    bumping version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer  "Needle Ops"
 maintainer_email "ops@needle.com"
 description "Deploys and configures Dreadnot from Rackspace"
 license "Apache 2.0"
-version "0.0.8"
+version "0.1.0"
 
 %w{ apt needle-base deploy_wrapper node runit aws secrets discovery }.each do |cb|
   depends cb
