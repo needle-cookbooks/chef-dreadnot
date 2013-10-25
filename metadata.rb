d4c4aa5f7cbc44e9139071379cecc693c995f98b
David Thornton <david@needle.com>
2013-10-25 11:42:32 -0600

    fixed typo

--- metadata.rb
+++ metadata.rb
@@ -2,10 +2,11 @@ maintainer  "Needle Ops"
 maintainer_email "ops@needle.com"
 description "Deploys and configures Dreadnot from Rackspace"
 license "Apache 2.0"
-version "0.1.2
+version "0.1.2"
 
 %w{ apt deploy_wrapper node runit aws secrets needle-base sysctl }.each do |cb|
   depends cb
 end
 
 depends "discovery", "~> 0.2.1"
+
