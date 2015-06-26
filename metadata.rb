35c6038397ecc5148dac5e7cf2aeaf25d972584a
David Thornton <david@needle.com>
2015-06-26 14:32:28 -0600

    added name to metadata and bumped version to 0.1.4

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name 'dreadnot'
 maintainer  "Needle Ops"
 maintainer_email "ops@needle.com"
 description "Deploys and configures Dreadnot from Rackspace"
 license "Apache 2.0"
-version "0.1.3"
+version "0.1.4"
 
 %w{ apt deploy_wrapper node runit aws secrets needle-base sysctl }.each do |cb|
   depends cb
